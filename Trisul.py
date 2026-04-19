d481781f1303ea52552d657e1525ece6d70ce53e
Anand Upadhayay <adriupadhyay@gmail.com>
2026-04-19 19:47:25 +0530

    Create Trisul.py

---
+++ Trisul.py
@@ -0,0 +1,17 @@
+# NMIT Hacks 2026 - My First Project
+# This code calculates the area of a circle
+
+def calculate_area(radius):
+    pi = 3.14
+    
+    # MISTAKE 1: Missing closing parenthesis ')'
+    print("Starting calculation for radius: " + str(radius)
+    
+    # MISTAKE 2: Logic error (using the wrong formula)
+    area = pi + radius + radius 
+    
+    return area
+
+# MISTAKE 3: Undefined variable 'r' (should be 'radius_value')
+radius_value = 5
+print("The area is:", calculate_area(r))
